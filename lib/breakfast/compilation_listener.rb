devlocker/breakfast

commit 5b7c6c9cf21efd9639b046efc84d7f2820987e4d

Add acceptance tests for compilation listener

lib/breakfast/compilation_listener.rb
@@ -10,10 +10,7 @@ module Breakfast
 
           ASSET_EXTENSIONS.each do |extension|
             if files.any? { |file| file.match(/\.#{extension}/) }
-              ActionCable.server.broadcast(
-                Breakfast::RELOAD_CHANNEL,
-                { extension: extension }
-              )
+              broadcast(Breakfast::RELOAD_CHANNEL, { extension: extension })
             end
           end
         end
@@ -25,17 +22,14 @@ module Breakfast
           SOURCE_CODE_EXTENSIONS.each do |extension|
             matched = files.select { |file| file.match(/\.#{extension}/) }
             if matched.present?
-              ActionCable.server.broadcast(
-                Breakfast::RELOAD_CHANNEL,
-                { extension: extension }
-              )
+              broadcast(Breakfast::RELOAD_CHANNEL, { extension: extension })
 
               file_names = matched
                 .map { |file| file.split("/").last }
                 .join(", ")
                 .truncate(60)
 
-              ActionCable.server.broadcast(Breakfast::STATUS_CHANNEL, {
+              broadcast(Breakfast::STATUS_CHANNEL, {
                 status: "success",
                 message: "saved: #{file_names}",
                 extension: extension
@@ -47,5 +41,9 @@ module Breakfast
       asset_listener.start
       rails_listener.start
     end
+  end
+
+  def self.broadcast(channel, data)
+    ActionCable.server.broadcast(channel, data)
   end
 end

spec/breakfast/compilation_listener_spec.rb
@@ -1,0 +1,58 @@
+require "spec_helper"
+require "tmpdir"
+
+RSpec.describe Breakfast::CompilationListener do
+  let!(:asset_dir) { Dir.mktmpdir }
+  let!(:source_code_dir) { Dir.mktmpdir }
+
+  before do
+    allow(Breakfast::CompilationListener).to receive(:broadcast)
+  end
+
+  describe ".start" do
+    it "will listen for changes to the asset output folder" do
+      Breakfast::CompilationListener.start(
+        asset_output_folders: asset_dir,
+        source_code_folders: source_code_dir
+      )
+
+      ["css", "js"].each do |extension|
+        open("#{asset_dir}/foo.#{extension}", "w")
+
+        sleep(2)
+
+        expect(Breakfast::CompilationListener).
+          to have_received(:broadcast).with(
+            Breakfast::RELOAD_CHANNEL,
+            { extension: extension }
+          )
+      end
+    end
+
+    it "will listen for changes to the source code folders" do
+      Breakfast::CompilationListener.start(
+        asset_output_folders: asset_dir,
+        source_code_folders: source_code_dir
+      )
+
+      ["rb", "html", "haml", "slim"].each do |extension|
+        open("#{source_code_dir}/foo.#{extension}", "w")
+
+        sleep(2)
+
+        expect(Breakfast::CompilationListener).
+          to have_received(:broadcast).with(
+            Breakfast::RELOAD_CHANNEL, 
+            { extension: extension }
+          )
+
+        expect(Breakfast::CompilationListener).
+          to have_received(:broadcast).with(Breakfast::STATUS_CHANNEL, {
+            status: "success",
+            message: "saved: foo.#{extension}",
+            extension: extension
+          })
+      end
+    end
+  end
+end

commit b2cddb36e86b38e16d06f859407f7d3e2e5f3882

Fix error caused by 5b7c6c9cf21efd9639b046efc84d7f2820987e4d
Moved the function outside the scope of the class, causing a no method
error.

lib/breakfast/compilation_listener.rb
@@ -41,9 +41,9 @@ module Breakfast
       asset_listener.start
       rails_listener.start
     end
-  end
 
-  def self.broadcast(channel, data)
-    ActionCable.server.broadcast(channel, data)
+    def self.broadcast(channel, data)
+      ActionCable.server.broadcast(channel, data)
+    end
   end
 end